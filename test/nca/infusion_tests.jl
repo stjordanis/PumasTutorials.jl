132eb59586afae444cdb8c066fe03607b6e2be6a
Yingbo Ma <mayingbo5@gmail.com>
2020-02-10 19:42:37 -0500

    Add tests

--- test/nca/infusion_tests.jl
+++ test/nca/infusion_tests.jl
@@ -11,3 +11,24 @@ data = @test_nowarn read_nca(df, id=:ID, time=:Time, conc=:Prefilter_Conc, verbo
 @test NCA.mrt(data; auctype=:last)[!, :mrt] == NCA.aumclast(data)[!, :aumclast]./NCA.auclast(data)[!, :auclast] .- 0.25timeu/2
 @test NCA.mrt(data; auctype=:inf)[!, :mrt] == NCA.aumc(data)[!, :aumc]./NCA.auc(data)[!, :auc] .- 0.25timeu/2
 @test !ismissing(NCA.vss(data)[!, 1])
+
+inf_data = CSV.read(IOBuffer("""
+id,time,conc,amt,duration,rate,route
+1,0,0,2551,0.5,5101,inf
+1,0.25,85.13,,,,
+1,0.416666667,109.31,,,,
+1,0.583333333,46.49,,,,
+1,0.666666667,24.66,,,,
+1,0.75,15.74,,,,
+1,1,9.86,,,,
+1,1.5,2.94,,,,
+1,2,2.4,,,,
+1,2.5,1.04,,,,
+1,4.5,0.21,,,,
+1,6.5,0.08,,,,
+1,8.5,0.1,,,,
+1,24.5,,,,,
+"""))
+@test inf_data.route[1] === Inf
+dfnca = @test_nowarn read_nca(inf_data, id=:id, time=:time, conc=:conc)
+@test dfnca[1].dose.formulation === NCA.IVInfusion
